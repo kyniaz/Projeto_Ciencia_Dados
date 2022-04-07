9ed1c8e9ecf926b3ac86a956a90e5994abcf4dd6
Kyniaz <oandre717@gmail.com>
2022-04-07 15:35:59 -0300

    --Mudanças na parte de predição.

--- app.R
+++ app.R
@@ -5,20 +5,8 @@ library(data.table)
 library(dplyr)
 library(bs4Dash)
 library(shinybusy)
-library(readr)
-library(fpp3)
-library(fpp2)
 library(forecast)
-library(fable)
-library(dplyr)
-library(tsibble)
-library(ggplot2)
-library(gridExtra)
-library(randtests)
-library(xts)
 library(zoo)
-library(TSA)
-library(astsa)
 options(scipen = 9999)
 
 #SAIDASUSPEITALTAS
@@ -37,7 +25,8 @@ ui <- dashboardPage(
             )
         ),
         dashboardBody(
-            uiOutput("escolhe_info"), box(width = 12,plotOutput("teste"), box(width = 12,plotOutput("serie_temporal")))
+            uiOutput("escolhe_info"), #box(width = 12,plotOutput("teste"), 
+            box(width = 12,plotOutput("serie_temporal"))
         )
   )
 
@@ -80,8 +69,7 @@ server <- function(input, output) {
                             estado, estadoNotificacao, municipio, municipioNotificacao)
     
     dataset$dados$Safra = as.factor(year(dataset$dados$dataNotificacao)*100 + month(dataset$dados$dataNotificacao))
-    dataset$dados$Semana = as.factor(year(dataset$dados$dataNotificacao)*10000 + month(dataset$dados$dataNotificacao)*100 + 
-    week(dataset$dados$dataNotificacao))
+    dataset$dados$Semana = as.factor(week(dataset$dados$dataNotificacao))
     dataset$dados$Dia = as.factor(yday(dataset$dados$dataNotificacao))
     
     output$escolhe_info = renderUI(
@@ -104,21 +92,25 @@ server <- function(input, output) {
                                    "saidaSuspeitaObitos",
                                    "saidaSuspeitaAltas",
                                    "saidaConfirmadaObitos",
-                                   "saidaConfirmadaAltas"))))
+                                   "saidaConfirmadaAltas"))),
+            column(6, sliderInput("previsao_prazo",
+                                  "Selecione a previsão(para 2022 nas visões de Dia e Semana):",
+                                  min = 1, max = 60, value = 1))
+            )
         
       )
     )
   })
   
   eventos = reactive({
-    list(input$data_tipo, input$info)
+    list(input$data_tipo, input$info, input$previsao_prazo)
   })
   
   observeEvent(eventos(), {
     if(!is.null(input$info) & !is.null(input$data_tipo)){
 
       if(input$data_tipo == "Por Semana") var_data = "Semana"
-      else if(input$data_tipo == "Por Dia") var_data='Dia'
+      else if(input$data_tipo == "Por Dia") var_data = "Dia"
       else var_data = "Safra"
 
       variavel = input$info
@@ -140,45 +132,86 @@ server <- function(input, output) {
       
       eixo_y = sub(strsplit(eixo_y,'')[[1]][1], toupper(strsplit(eixo_y,'')[[1]][1]), eixo_y)
       
-      if(input$data_tipo == "Por Dia")
-      eval(parse(text = paste0('total_algo_safra = ggplot(Y) +
-        geom_line(size = 1, aes(x = ',var_data,', y = Total, group = 1), col = "springgreen3") +
-        theme_minimal() +
-        theme(axis.text.x = element_blank()) + 
-        xlab("Tempo") +
-        ylab(eixo_y)
-      ')))
-      else 
-        eval(parse(text = paste0('total_algo_safra = ggplot(Y) +
-        geom_line(size = 1, aes(x = ',var_data,', y = Total, group = 1), col = "springgreen3") +
-        theme_minimal() +
-        theme(axis.text.x = element_text(angle=90,hjust=1)) + 
-        xlab("Tempo") +
-        ylab(eixo_y)
-      ')))
+      # if(input$data_tipo == "Por Dia")
+      # eval(parse(text = paste0('total_algo_safra = ggplot(Y) +
+      #   geom_line(size = 1, aes(x = ',var_data,', y = Total, group = 1), col = "springgreen3") +
+      #   theme_minimal() +
+      #   theme(axis.text.x = element_blank()) + 
+      #   xlab("Tempo") +
+      #   ylab(eixo_y)
+      # ')))
+      # else 
+      #   eval(parse(text = paste0('total_algo_safra = ggplot(Y) +
+      #   geom_line(size = 1, aes(x = ',var_data,', y = Total, group = 1), col = "springgreen3") +
+      #   theme_minimal() +
+      #   theme(axis.text.x = element_text(angle=90,hjust=1)) + 
+      #   xlab("Tempo") +
+      #   ylab(eixo_y)
+      # ')))
       
-      if(input$data_tipo == "Por Dia")
-      eval(parse(text = paste0("serie <- ts(Y$Total, start = 1, frequency=7)")))
-      else if (input$data_tipo == "Por semana")
-      eval(parse(text = paste0("serie <- ts(Y$Total, start = 1, frequency=30)")))
-      else 
-      eval(parse(text = paste0("serie <- ts(Y$Total, start = 1, frequency=1)"))) 
+      dia_mais_recente = as.Date(max(dataset$dados$dataNotificacao),'%Y-%m-%d')
       
+      if(input$data_tipo == "Por Dia"){
+        prev_prazo = input$previsao_prazo
+        if( input$ano == '2022')
+          eval(parse(text = paste0("serie = zoo(Y$Total, seq(from = as.Date('",input$ano,"-01-01'), to = dia_mais_recente, by = 1))")))
+        else{
+          eval(parse(text = paste0("serie = zoo(Y$Total, seq(from = as.Date('",input$ano,"-01-01'), to = as.Date('",input$ano,"-12-31'), by = 1))")))
+        }
+      }
+      else if (input$data_tipo == "Por Semana"){
+        prev_prazo = input$previsao_prazo
+        if( input$ano == '2022')
+          eval(parse(text = paste0("serie = zoo(Y$Total, seq(from = as.Date('",input$ano,"-01-01'), to = dia_mais_recente, by = 7))")))
+        else{
+          eval(parse(text = paste0("serie = zoo(Y$Total, seq(from = as.Date('",input$ano,"-01-01'), to = as.Date('",input$ano,"-12-31'), by = 7))")))
+        }
+      }
+      else {
+        prev_prazo = 1
+        if( input$ano == '2022')
+          eval(parse(text = paste0("serie = zoo(Y$Total, seq(from = as.Date('",input$ano,"-01-01'), to = dia_mais_recente, by = 30))")))
+        else{
+          eval(parse(text = paste0("serie = zoo(Y$Total, seq(from = as.Date('",input$ano,"-01-01'), to = as.Date('",input$ano,"-12-31'), by = 30))")))
+        }
+      }
+
       fit = auto.arima(serie)
       
-      serie_temporal <- 
-        autoplot(forecast(fit, level=0.9)) +
-        theme_minimal() +
-        theme(axis.text.x = element_text(angle=90,hjust=1), legend.position = 'bottom') + 
-        xlab("Tempo") +
-        ylab(eixo_y)
+      if(input$ano == '2022'){
+
+        predicao = forecast(fit, h = prev_prazo)
+        datas_predicao = row.names(print(predicao))
+        datas = as.Date(row.names(data.frame(serie)))
         
+        serie_temporal <- 
+          ggplot() +
+          geom_line(aes(x = datas, y = Y$Total, colour = "a")) +
+          geom_line(aes(x = datas, y = fit$fitted, colour = "b")) + 
+          geom_line(aes(x = as.Date(as.numeric(datas_predicao)), y = predicao$mean, colour = "c")) +
+          geom_ribbon(aes(x = as.Date(as.numeric(datas_predicao)), 
+                        ymin = predicao$lower[,2], ymax = predicao$upper[,2], fill = "a"), alpha = 0.5) +
+          theme_minimal() +
+          theme(axis.text.x  = element_text(angle=90,hjust=1), legend.position = 'bottom') + 
+          xlab("Tempo") +
+          scale_colour_manual(name = "Cor:", values = c("royalblue","springgreen4","red2"), 
+                              labels = c("Observado","Modelo ARIMA ajustado","Previsão")) +
+          scale_fill_manual(name = NULL, values = "red2", labels = "Predição (95%)") +
+          ylab(eixo_y)        
+      }
+      else {
+        serie_temporal <- 
+          autoplot(serie) +
+          theme_minimal() +
+          theme(axis.text.x = element_text(angle=90,hjust=1), legend.position = 'bottom') + 
+          xlab("Tempo") +
+          ylab(eixo_y)
+      }
       
-      
-      output$teste = renderPlot(total_algo_safra)
+      #output$teste = renderPlot(total_algo_safra)
       output$serie_temporal = renderPlot(serie_temporal)
     }
-  })
+  }, ignoreInit = T)
 }
 
 # Run the application 
